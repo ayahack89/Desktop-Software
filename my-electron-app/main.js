0e27422e9ec338ac0340f7354fd0ea55243d61c4
Ayanavbha Chatterjee <117476380+aYgCOO@users.noreply.github.com>
2024-08-25 01:31:45 +0530

    Update

--- my-electron-app/main.js
+++ my-electron-app/main.js
@@ -1,15 +1,29 @@
-const { app, BrowserWindow } = require('electron')
+// main.js
+const { app, BrowserWindow } = require('electron');
 
-const createWindow = () => {
+function createWindow() {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
-  })
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true, // Enable Node.js integration in renderer process
+      contextIsolation: false // Disable context isolation
+    }
+  });
 
-  win.loadFile('index.html')
+  win.loadFile('index.html');
 }
 
-app.whenReady().then(() => {
-  createWindow()
-})
- 
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
